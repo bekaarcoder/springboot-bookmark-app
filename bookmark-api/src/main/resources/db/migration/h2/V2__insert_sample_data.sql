bda79fbc44e15177436abf13e0448e176d21a316
Shashank <s_shashank@outlook.com>
2022-09-13 21:34:29 +0530

    updated sql files

---
+++ bookmark-api/src/main/resources/db/migration/h2/V2__insert_sample_data.sql
@@ -0,0 +1,8 @@
+insert into bookmark
+    (title, url, created_at)
+values
+    ('Google', 'https://www.google.com', CURRENT_TIMESTAMP()),
+    ('Youtube', 'https://www.youtube.com', CURRENT_TIMESTAMP()),
+    ('Twitter', 'https://www.twitter.com', CURRENT_TIMESTAMP()),
+    ('Yahoo', 'https://www.yahoo.com', CURRENT_TIMESTAMP()),
+    ('Netflix', 'https://www.netflix.com', CURRENT_TIMESTAMP());
--- bookmark-api/src/test/java/com/blitzstriker/bookmarkapi/BookmarkApiApplicationTests.java
+++ bookmark-api/src/test/java/com/blitzstriker/bookmarkapi/BookmarkApiApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookmarkApiApplicationTests {
+public class BookmarkApiApplicationTests {
 
     @Test
     void contextLoads() {
-    }
 
+    }
 }
